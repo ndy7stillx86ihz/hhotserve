ffb1dec4a92a179decb027d38ebd63fef3f9f42d
cmfernandez <cmfernandez@xetid.cu>
2025-08-31 09:18:38 -0400

    Enhance installation script to check for dependencies and prompt for installation

--- install.sh
+++ install.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+if ! command -v apt &> /dev/null; then
+    echo -e "\e[33mThis script is intended for Debian-based systems. Exiting.\e[0m"
+    exit 1
+fi
+if ! command -v python3 &> /dev/null; then
+    read -p "Python3 is not installed. Do you want to install it? (y/n): " choice
+    if [[ "$choice" == "y" ]]; then
+      sudo apt install python3 -y
+    else
+echo -e "\e[33mPython3 is required. Exiting.\e[0m"
+        exit 1
+    fi
+fi
+if ! command -v watchmedo &> /dev/null; then
+    read -p "watchdog is not installed. Do you want to install it? (y/n): " choice
+    if [[ "$choice" == "y" ]]; then
+      sudo apt install python3-watchdog -y
+    else
+        echo -e "\e[33mwatchdog python dependency is required. Exiting.\e[0m"
+        exit 1
+    fi
+fi
+if ! command -v git &> /dev/null; then
+    read -p "git is not installed. Do you want to install it? (y/n): " choice
+    if [[ "$choice" == "y" ]]; then
+      sudo apt install git -y
+    else
+        echo -e "\e[33mgit is required. Exiting.\e[0m"
+        exit 1
+    fi
+fi
+
 mkdir -p ~/.local/opt/hhotserve
 cd ~/.local/opt/hhotserve || exit
 git clone https://github.com/ndy7stillx86ihz/hhotserve.git
@@ -12,4 +44,4 @@ if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
     echo "Added ~/.local/bin to PATH. Please restart your terminal or run 'source ~/.bashrc' or 'source ~/.zshrc'."
 fi
 
-echo "Installation complete. You can run the program using the command 'hhotserve'."
+echo -e "\e[32mInstallation complete. You can run the program using the command 'hhotserve'.\e[0m"
